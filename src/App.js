bbc8ca1da5fd95c24b3164dbf9a61cfa3b3a0be6
lfe9142 <lflippo001@gmail.com>
2018-08-28 23:31:16 -0500

    adding to list now works

--- src/App.js
+++ src/App.js
@@ -5,28 +5,32 @@ import './App.css';
 class App extends Component {
   state = {
     tasks: [],
+    currentFormVaule: ''
   };
+
+  handleTextChange(e) {
+    console.log("text", e.target.value);
+    this.setState({currentFormVaule: e.target.value});
+  }
 
   handleAddTask() {
     const newTaskList = this.state.tasks.slice();
-    newTaskList.push("add task");
+    newTaskList.push(this.state.currentFormVaule);
     this.setState({tasks: newTaskList});
     console.log(this.state.tasks);
   }
 
   renderList() {
-
-    const list = this.state.tasks.map((item) => {
+    return this.state.tasks.map((item) => {
       return (<li>{item}</li>);
     });
-    console.log("render list", list);
-    return list;
   }
 
   render() {
     return (
       <div>
         <h2>Todo List</h2>
+        <input type="text" onChange={(e) => {this.handleTextChange(e)}}/>
         <button onClick={()=> this.handleAddTask()}>Add task</button>
         <ul>{this.renderList()}</ul>
       </div>
